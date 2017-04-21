4c1e1e9cf2fa7a02da36c7c0323e64a29262828d
tran phuoc <thephuoc2101@gmail.com>
2017-04-21 20:14:16 +0700

    new

---
+++ AssignmentFinal_ps03948/include/footer.php
@@ -0,0 +1,10 @@
+<embed id="Advertisement" 
+width="960" height="100" 
+flashvars="clickTARGET=_blank&clickTAG=#" 
+allowscriptaccess="always"
+wmode="transparent" quality="high" 
+name="Advertisement" style="admincp" 
+src="images/quangcao.swf" 
+type="application/x-shockwave-flash">
+Đồ án Website kinh doanh máy tính xách tay trực tuyến</br>
+Thực hiện: Nguyễn Xuân Quốc - CCHT05A020 - XuanQuocQN@gmail.com - 01657990372
---
+++ AssignmentFinal_ps03948/include/gioithieu.php
@@ -0,0 +1,58 @@
+<style type="text/css">
+<!--
+.style1 {
+	color: #0000FF;
+	font-weight: bold;
+}
+.style2 {
+	color: #FF0000;
+	font-weight: bold;
+}
+.style8 {
+	font-size: 18px;
+	color: #FF00FF;
+	font-weight: bold;
+}
+-->
+</style>
+<div align="justify" style="width:550;line-height:20px">
+<div align="center">
+<div align="center" style="line-height:25px ">
+ <span class="style1">ĐỒ ÁN LẬP TRÌNH WEB</span></div>  
+   <div class="style8" align="center"><br />
+  WEBSITE KINH DOANH MÁY TÍNH TRỰC TUYẾN</div>
+   <p><br>
+  <span class="style2">Chuyên kinh doanh các dòng laptop từ các thương hiệu nổi tiếng với mức giá rẻ nhất thị trường và chất lượng hàng đầu.</span></p>
+   <p><br>
+   </p>
+  <div align="right"><span class="style1"><br>
+    <br>  
+    <br>
+</span></div>
+
+  <table width="550" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td width="200"  rowspan="3" align="right" style="border-right:1px dotted #CCCCCC"><div style="font-size:18px;"><strong>Số điện thoại:</strong></div></td>
+      <td style="color:#0000FF; font-size:18px; padding-left:30px"><strong><span style="font-size: 18px; color: #FF0000">0903156306</span></strong></td>      
+    </tr>
+    <tr>
+      <td style="color:#0000FF; font-size:18px; padding-left:30px"><span style="color:#FF0000; font-size:18px"><strong>01657990372</strong></span></td>
+    </tr>
+     <tr>
+      <td style="color:#0000FF; font-size:18px; padding-left:30px">&nbsp;</td>
+    </tr>
+   <tr><td colspan="2"><hr /></td></tr>
+    <tr>
+      <td width="200"  rowspan="3" align="right" style="border-right:1px dotted #CCCCCC"><div style="font-size:18px;"><strong>Hỗ trợ online:</strong></div></td>
+      <td style="color:#0000FF; font-size:18px; padding-left:30px"><span class="title">XuanQuocQN@gmail.com</span></font></font></td>      
+    </tr>
+    <tr>
+      <td style="color:#0000FF; font-size:18px; padding-left:30px">PhuongLe.QN.23@gmail.com</span></font></font></td>
+    </tr>
+     <tr>
+      <td style="color:#0000FF; font-size:18px; padding-left:30px">&nbsp;</td>
+    </tr>
+    </table>
+ 
+
+</div>
